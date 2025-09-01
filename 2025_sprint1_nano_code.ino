293326d83bf0d9bec46437a3521e2d42bc730451
gnarl <43803+gnarl@users.noreply.github.com>
2025-09-01 13:42:48 -0400

    Flashing LEDs working

--- 2025_sprint1_nano_code.ino
+++ 2025_sprint1_nano_code.ino
@@ -5,26 +5,39 @@
 
 Adafruit_NeoPixel strip(LED_COUNT, NEOPIXEL_CONTROL_PIN, NEO_GRB + NEO_KHZ800);
 
+const uint32_t BLUE = strip.Color(0, 0, 255);
+const uint32_t RED = strip.Color(255, 0, 0);
+const uint32_t GREEN = strip.Color(0, 255, 0);
+uint32_t currentColor = GREEN;
+
+
 void setup() {
   strip.begin();
-  
 }
 
 void loop() {
-  // Reset all pixels to off. Does not change LED color unitl show() is called.
+  // Reset all pixels to off. Does not change LED color until show() is called.
   strip.clear();
 
   // Set the first 4 LEDs to red
   for(int i=0; i<LED_COUNT; i++) {
-    strip.setPixelColor(i, strip.Color(255, 0, 0)); // Red
+    strip.setPixelColor(i, currentColor); // Red
   }
   strip.show();
   delay(1000); // Keep them on for 1 second
+  changeColor();
 
-  // Turn the LEDs off
-  for(int i=0; i<LED_COUNT; i++) {
-    strip.setPixelColor(i, strip.Color(0, 0, 0)); // Off
-  }
+  strip.clear();
   strip.show();
   delay(1000); // Wait 1 second
+}
+
+void changeColor() {
+  if (currentColor == GREEN) {
+    currentColor = BLUE;
+  } else if (currentColor == BLUE) {
+    currentColor = RED;
+  } else {
+    currentColor = GREEN;
+  }
 }
--- GEMINI.md
+++ GEMINI.md
@@ -9,11 +9,19 @@ To build and run this project, you will need the Arduino IDE.
 1.  Open the Arduino IDE.
 2.  Open the `2025_sprint1_nano_code.ino` file.
 3.  Connect your Arduino Nano to your computer.
-4.  Select the correct board and port from the `Tools` menu.
-5.  Click the `Upload` button to build and upload the sketch to the board.
+4.  Select `Arduino Nano` as the board from the `Tools` menu.
+5.  Select `ATmega328P (Old Bootloader)` as the processor from the `Tools` menu.
+6.  Click the `Upload` button to build and upload the sketch to the board.
+
+## Determine Arduino Port
+1. Before connecting the Nano V3.0 via USB run `ls /dev/cu.*` to display the existing ports.
+2. Connect the Nano v3.0 via USB, wait for board to initialize, an run `ls /dev/cu.*` again.
+3. Set the newly discovered port in the `Tools` menu of the Arduino IDE. 
 
 # Development Conventions
 
 *   The main source code is in the `.ino` file.
 *   The `setup()` function is used for initialization.
 *   The `loop()` function is used for the main program logic.
+
+
